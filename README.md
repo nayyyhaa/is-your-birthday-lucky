f695971529e9f640a573003941c261bb0cc4bfdf
nayyyhaa <nehaguptang.ng@gmail.com>
2021-09-14 00:02:14 +0530

    add: project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Is-your-birthday-lucky
+
+- This is Vanilla Js project to validate if your birthday is lucky or not.
---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+let dateOfBirth = document.querySelector('.dob-input');
+let luckyNum = document.querySelector('.lucky-num-input');
+let submitBtn = document.querySelector('.submit-btn');
+let output = document.querySelector('.output');
+let privacyAlert = document.querySelector('#privacyAlert');
+let privacyText = document.querySelector('#privacyPolicyText');
+let closeIcon = document.querySelector('.fas.fa-times');
+let messages = ['Yay! Your Birthday is lucky.', 'Oopsie! Your birthday is not lucky.', 'Please enter valid Inputs!'];
+let messageIndex;
+//Functions
+
+function validateInputFields() {
+    if(dateOfBirth.value && luckyNum.value) return true;
+    return false;
+}
+
+function getDateSum() {
+    let dateString = dateOfBirth.value.replace(/-/g,'').split('');
+    return dateString.reduce((total,el) => total+Number(el),0)
+}
+
+function displayOutput() {
+    output.innerText = messages[messageIndex];
+}
+
+function validateLuckyBirthday() {
+    if(validateInputFields()) {
+        let sum = getDateSum();
+        if(sum % luckyNum.value === 0) messageIndex = 0;
+        else messageIndex = 1;
+    }
+    else messageIndex = 2;
+    displayOutput();
+}
+
+function showPrivacyPolicy() {
+    privacyAlert.classList.add('-active');
+}
+
+function closePrivacyPolicy() {
+    privacyAlert.classList.remove('-active');
+}
+
+//Event listeners
+
+submitBtn.addEventListener('click',validateLuckyBirthday);
+privacyText.addEventListener('click', showPrivacyPolicy);
+closeIcon.addEventListener('click', closePrivacyPolicy);
